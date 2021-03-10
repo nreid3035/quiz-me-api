10aaf68330308502865237f6d48050303a5c49a2
Nicholas Reid <nicholasreid845@gmail.com>
2021-03-10 12:56:39 -0500

    added dependencies, arranged files

--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
+const knex = require('knex')
 const app = require('./app')
+const pg = require('pg')
+pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
 
-const { PORT } = require('./config')
+
+const { PORT, DB_URL } = require('./config')
+
+const db = knex({
+    client: 'pg',
+    connection: DB_URL
+})
+
+app.set('db', db)
 
 app.listen(PORT, () => {
     console.log(`Server listening at ${PORT}`)
